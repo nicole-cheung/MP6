6e81810eb8472249f22e7415f18ed10d2f3bef9e
nicolecheung <ncheung3@illinois.edu>
2018-12-05 09:39:37 -0600

    Buttons can redirect between activities.

--- app/src/main/java/com/example/nicolecheung/myapplication/ContactPage.java
+++ app/src/main/java/com/example/nicolecheung/myapplication/ContactPage.java
@@ -1,13 +1,23 @@
 package com.example.nicolecheung.myapplication;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.util.Log;
+import android.widget.Button;
 
 public class ContactPage extends AppCompatActivity {
+
+    private static final String TAG = "ContactPage";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_contact_page);
+    }
+    public void start(View v) {
+        Intent i = new Intent (this, MainActivity.class);
+        startActivity(i);
     }
 }
--- app/src/main/java/com/example/nicolecheung/myapplication/MainActivity.java
+++ app/src/main/java/com/example/nicolecheung/myapplication/MainActivity.java
@@ -4,8 +4,12 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.content.Intent;
+import android.util.Log;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    private static final String TAG = "MainActivity";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -15,5 +19,7 @@ public class MainActivity extends AppCompatActivity {
     public void start(View v) {
         Intent i = new Intent (this, ContactPage.class);
         startActivity(i);
+
     }
+
 }
